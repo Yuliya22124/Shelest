e6a4b053866c9bf22b3a6d5c0ff036b9a81a9e70
337 <337@kp15.lan>
2022-04-19 12:31:59 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Data.Sql;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using ПШ.Utils;
+
+namespace ПШ
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            var Agents = DatabaseContext.db.Agent.ToList();
+            InitializeComponent();
+            LViewAgents.ItemsSource = Agents;
+        }
+    }
+}
---
+++ Utils/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПШ.Utils
+{
+    public class DatabaseContext
+    {
+        public static BaseModel db = new BaseModel();
+    }
+}
